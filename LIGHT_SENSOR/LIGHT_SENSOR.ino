3c634f6bc32c58259a17e7a5ca378b21e27d6bec
Jack <54298425+DizzyFop@users.noreply.github.com>
2025-01-23 15:22:21 -0600

    Light with text output

--- LIGHT_SENSOR/LIGHT_SENSOR.ino
+++ LIGHT_SENSOR/LIGHT_SENSOR.ino
@@ -1,9 +1,25 @@
 void setup() {
   // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  
   pinMode(A6, INPUT);
   int lightLevel = analogRead(A6);
   Serial.begin(9600);
+  Serial.println();
   Serial.println(lightLevel);
+  if (lightLevel < 100){
+    Serial.print("It’s really dark!");
+  }
+  else if (lightLevel < 200){
+    Serial.print("It’s dim in here");
+  }
+  else if (lightLevel < 700){
+    Serial.print("It’s a little bright");
+  }
+  else if (lightLevel < 1024){
+    Serial.print("It’s really bright!");
+  }
 }
 
 void loop() {
